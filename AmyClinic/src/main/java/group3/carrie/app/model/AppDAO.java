65d8aa34feeaf72c2139e917b5e83cf37a543e2d
Carrie <kniver999@gmail.com>
2016-04-21 07:50:24 +0800

    新增AppDAO_interface、AppDAO、AppService、AppVO

---
+++ AmyClinic/src/main/java/group3/carrie/app/model/AppDAO.java
@@ -0,0 +1,37 @@
+package group3.carrie.app.model;
+
+import java.util.List;
+
+public class AppDAO implements AppDAO_interface {
+
+	@Override
+	public void insert(AppVO appVO) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(AppVO appVO) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer aid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public AppVO findByPrimaryKey(Integer aid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AppVO> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
