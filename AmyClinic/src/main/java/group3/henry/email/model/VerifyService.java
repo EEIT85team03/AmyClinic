d2d55e6d5d2a3441c0eb60edb0632cd3e6c27e70
Henry <eeit85group3@gmail.com>
2016-05-11 18:10:16 +0800

    Modified VerifyService to store member info in session for jsp use

--- AmyClinic/src/main/java/group3/henry/email/model/VerifyService.java
+++ AmyClinic/src/main/java/group3/henry/email/model/VerifyService.java
@@ -5,6 +5,11 @@ import group3.henry.login.model.MemberVO;
 
 import java.util.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
 public class VerifyService {
 	static private MemberDAO dao = new MemberDAO();
 	static private List<MemberVO> memberList = new ArrayList<MemberVO>();	
@@ -19,6 +24,9 @@ public class VerifyService {
 		for (MemberVO mb : memberList) {
 			if (mb.getVerify()!=null || !mb.getVerify().isEmpty()){
 				if (mb.getEmail().trim().equals(email.trim()) && mb.getVerify().trim().equals(token.trim()) ) {
+					HttpServletRequest request = ServletActionContext.getRequest();
+					HttpSession session = request.getSession();
+					session.setAttribute("memberVO", mb);
 					mb.setAct_status(1);
 					mb.setVerify("");
 					dao.update(mb);
