4220a6ad5453e871fd659cbcdf0c8d6546371633
Jgkfu <kismypsy@yahoo.com>
2016-05-18 00:21:54 +0800

    Product

--- AmyClinic/src/main/java/group3/carrie/product/model/ProductDAO.java
+++ AmyClinic/src/main/java/group3/carrie/product/model/ProductDAO.java
@@ -19,7 +19,7 @@ import org.hibernate.Session;
 
 public class ProductDAO implements ProductDAO_interface {
 	private static final String GET_ALL_STMT = "FROM ProductVO order by pid";
-//	private static final String GET_ODItems_BYPid_STMT = "FROM OrderItemsVO where pid = ?";
+	private static final String GET_ODItems_BYPid_STMT = "FROM OrderItemsVO where pid = ?";
 	private static final String DELETE_ITEM = "DELETE FROM OrderItemsVO where pid = ?";
 	private static final String DELETE_PROD = "DELETE FROM ProductVO where pid = ?";
 
@@ -133,10 +133,10 @@ public class ProductDAO implements ProductDAO_interface {
 		return set;
 	}
 
-	public static void main(String[] args) {
-		ProductDAO dao = new ProductDAO();
-
-		// 新增
+//	public static void main(String[] args) {
+//		ProductDAO dao = new ProductDAO();
+//
+//		// 新增
 //		ProductVO productVO1 = new ProductVO();
 //		CatagoryVO catagoryVO = new CatagoryVO();
 //		productVO1.setName("測試用111");
@@ -224,29 +224,29 @@ public class ProductDAO implements ProductDAO_interface {
 //		}
 
 		// 查訂單明細
-		List<ProductVO> list = dao.getAll();
-		for (ProductVO product : list) {
-			System.out.print(product.getPid() + ",");
-			System.out.print(product.getName() + ",");
-			System.out.print(product.getPhoto() + ",");
-			System.out.print(product.getAmount() + ",");
-			System.out.print(product.getCatagoryVO().getCid() + ",");
-			System.out.print(product.getCatagoryVO().getName() + ",");
-			System.out.print(product.getPrice() + ",");
-			System.out.print(product.getDiscount() + ",");
-			System.out.print(product.getDescrip() + ",");
-			System.out.print(product.getIngredients());
-			System.out.println("\n---------------------------------");
-		 Set<OrderItemsVO> set = product.getOrderItems();
-		 	for (OrderItemsVO orderItems : set) {
-		 		System.out.print(orderItems.getId() + ",");
-		 		System.out.print(orderItems.getProductVO().getName() + ",");
-		 		System.out.print(orderItems.getQuantity() + ",");
-		 		System.out.print(orderItems.getPrice_per());
-		 		System.out.println();
-		 	}
-		 	System.out.println();
-		}
-	}
+//		List<ProductVO> list = dao.getAll();
+//		for (ProductVO product : list) {
+//			System.out.print(product.getPid() + ",");
+//			System.out.print(product.getName() + ",");
+//			System.out.print(product.getPhoto() + ",");
+//			System.out.print(product.getAmount() + ",");
+//			System.out.print(product.getCatagoryVO().getCid() + ",");
+//			System.out.print(product.getCatagoryVO().getName() + ",");
+//			System.out.print(product.getPrice() + ",");
+//			System.out.print(product.getDiscount() + ",");
+//			System.out.print(product.getDescrip() + ",");
+//			System.out.print(product.getIngredients());
+//			System.out.println("\n---------------------------------");
+//		 Set<OrderItemsVO> set = product.getOrderItems();
+//		 	for (OrderItemsVO orderItems : set) {
+//		 		System.out.print(orderItems.getId() + ",");
+//		 		System.out.print(orderItems.getProductVO().getName() + ",");
+//		 		System.out.print(orderItems.getQuantity() + ",");
+//		 		System.out.print(orderItems.getPrice_per());
+//		 		System.out.println();
+//		 	}
+//		 	System.out.println();
+//		}
+//	}
 
 }
--- AmyClinic/src/main/webapp/Backstage/Template.jsp
+++ AmyClinic/src/main/webapp/Backstage/Template.jsp
@@ -6,53 +6,29 @@
 <html>
 <head>
 <meta charset="utf-8">
-
 <title>-AMY-</title>
 <link href="css/backstage.css" rel="stylesheet"/>
-
 <link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css">
-<script src="js/jquery-1.12.3.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+<%-- <script src="js/jquery-1.12.3.min.js"></script> --%>
+<%-- <script src="js/bootstrap.min.js"></script> --%>
 <script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
 <s:head theme="xhtml" /><!-- 預設 -->
 <sx:head debug="false" cache="false" parseContent="false"	compressed="false" extraLocales="zh-tw,en-us,ja,ko" />
-<style type="text/css">
-
-</style>
 </head>
 
 <body>
-
-<div class="container" align="center" >
+<div class="container">
 <jsp:include page="placeJsp/top.jsp" /><jsp:include page="placeJsp/sidebar1.jsp" />
-
-
   <div class="content">
-    <h1 align="center"  style="color: blue">AMY</h1>
-    
-<script type="text/javascript">
-    var d = new Date();
-    var theYear = d.getFullYear()-1911;
-    var theMonth = d.getMonth()+1;
-    var theDate = d.getDate();
-    
-    document.write("民國"+theYear+"年"+theMonth+"月"+theDate+"日");
-</script>
-
-    <s:form action="LoginActionEmp"  namespace="/emplogpage">
-	  <s:textfield name="Employees.eid" label="員工編號" value="1" />
-<!--       編號<input type="text" name="eid" id="text1"> -->
-      <s:textfield name="Employees.name" label="員工姓名" value="Dr. Pocky" />
-<!--       姓名<input type="text" name="name" id="text2"> -->
-    <s:submit value="確定"  method="LoginServiceEmp"/>
-    </s:form>
-    <a href="#">忘記密碼</a>
-    <br>
-    <a>EX==>id=1,name=Dr. Pocky</a>
-    
-    <div align="center">
-      
-    </div>
+ <!-- 內文-->
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
     <!-- end .content --></div>
 <jsp:include page="placeJsp/footer.jsp" />
   <!-- end .container --></div>
