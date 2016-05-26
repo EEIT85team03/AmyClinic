b4f6489dcbf13449363b9da4d435033a583969cb
Henry <eeit85group3@gmail.com>
2016-05-26 16:46:51 +0800

    Code renovation for compatiability with Azure Cloud services

--- AmyClinic/src/main/java/group3/henry/login/controller/RecoverAction_old.java
+++ AmyClinic/src/main/java/group3/henry/login/controller/RecoverAction_old.java
@@ -8,7 +8,6 @@ import org.apache.struts2.ServletActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 import group3.henry.email.model.VerifyService;
-import group3.henry.global.utility.GlobalService;
 import group3.henry.global.utility.Mailer;
 import group3.henry.global.utility.TokenGenerator;
 import group3.henry.login.model.MemberServices;
@@ -34,7 +33,7 @@ Action sets new pw in DB
 redirect user to Index.jsp
 
 */
-public class RecoverAction extends ActionSupport implements GlobalService{
+public class RecoverAction_old extends ActionSupport {
 	private static final long serialVersionUID = 1L;
 	private static final String HEADER = "AmyClinic Password Recovery";
 
@@ -57,10 +56,11 @@ public class RecoverAction extends ActionSupport implements GlobalService{
 	public void setTempPW(String tempPW) {this.tempPW = tempPW;}
 	
 	private String compose(String token, String email){ // composes a message specific for recovering password
+		String nl = System.getProperty("line.separator");
 		System.out.println("RecoverAction compose()");
-		return "Someone requested a password reset at AmyClinic with your Email address!" + NL + 
-				"If this wasn't you, please ignore this email!" + NL + NL + "Please click the following link to reset your password!" +NL+NL+
-				"http://" + SERVER + "/AmyClinic/free/recover.action?token=" + token + "&email=" + email;			
+		return "Someone requested a password reset at AmyClinic with your Email address!" + nl + 
+				"If this wasn't you, please ignore this email!" + nl + nl + "Please click the following link to reset your password!" +nl+nl+
+				"http://localhost:8080/AmyClinic/free/recover.action?token=" + token + "&email=" + email;			
 	}
 	
 	public String recover() { // activates when a user requests a password reset (from recover.jsp)
